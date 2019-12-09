c690ca64f3bf43dcff38af73e9671f6879287b5e
andyzen619 <andyzen619@gmail.com>
2019-12-09 22:31:01 +0000

    Finished ex 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,30 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(
+  name: "Surrey", 
+  annual_revenue: 224000, 
+  womens_apparel: true,
+  mens_apparel: false
+)
+Store.create(
+  name: "Whistler", 
+  annual_revenue: 1900000, 
+  womens_apparel: false,
+  mens_apparel: true
+)
+
+Store.create(
+  name: "Yaletown", 
+  annual_revenue: 430000, 
+  womens_apparel: true,
+  mens_apparel: true
+)
+
+puts Store.count
+
+(1..Store.count).map{|store_id| @current = Store.find_by(id: store_id) 
+if @current
+  puts "#{@current.name}, #{@current.annual_revenue}"
+end
+}
